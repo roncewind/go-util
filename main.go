2c73a245ed30e41cea166ad5ae83107bb489c20c
roncewind <dad@lynntribe.net>
2023-02-10 13:58:34 -0800

    added test cases and examples

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/roncewind/go-util/util"
 )
@@ -15,22 +16,30 @@ func main() {
 
 // ----------------------------------------------------------------------------
 
-// Example of how to use OrDone()
+// Example of how to use OrDone()... a fairly silly example.
+// It creates a stream of ints, then prints the max that gets read in 100 microseconds
 func orDoneExample() {
 	ctx, cancel := context.WithCancel(context.Background())
-	intStream := make(chan interface{})
+	intStream := make(chan int)
+	defer close(intStream)
+
+	// goroutine that loads ints into a channel
 	go func() {
-		for i := 0; i < 20; i++ {
+		for i := 0; i < 500; i++ {
 			intStream <- i
 		}
 	}()
 
+	// goroutine waits 100 microseconds, then cancels the context
+	go func() {
+		time.Sleep(100 * time.Microsecond)
+		cancel()
+	}()
+
+	max := 0
 	// range over OrDone instead of implementing the for-select idiom
 	for val := range util.OrDone(ctx, intStream) {
-		fmt.Println(val)
-		if val == 10 {
-			cancel()
-		}
+		max = val
 	}
-
+	fmt.Println(max)
 }
---
+++ util/util_test.go
@@ -0,0 +1,66 @@
+package util
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+)
+
+// ----------------------------------------------------------------------------
+
+// test normal operation of OrDone
+func TestUtil_OrDone(t *testing.T) {
+	intCount := 1000
+	cancelled := false
+	ctx, cancel := context.WithCancel(context.Background())
+	intStream := make(chan int)
+
+	// goroutine that loads ints into a channel
+	go func() {
+		for i := 0; i < intCount; i++ {
+			intStream <- i
+		}
+	}()
+
+	// goroutine waits 100 microseconds, then cancels the context
+	go func() {
+		time.Sleep(100 * time.Microsecond)
+		cancelled = true
+		cancel()
+	}()
+
+	max := 0
+	// range over OrDone instead of implementing the for-select idiom
+	for val := range OrDone(ctx, intStream) {
+		max = val
+	}
+	fmt.Println(max, intCount)
+	if !cancelled {
+		t.Fatal("expected context to cancel before end of test")
+	}
+}
+
+// ----------------------------------------------------------------------------
+
+// test OrDone on closed stream
+func TestUtil_OrDone_closedStream(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	intStream := make(chan int)
+	close(intStream)
+
+	// goroutine waits 100 microseconds, then cancels the context
+	go func() {
+		time.Sleep(1000 * time.Microsecond)
+		cancel()
+	}()
+
+	max := 0
+	// range over OrDone instead of implementing the for-select idiom
+	for val := range OrDone(ctx, intStream) {
+		max = val
+		if max > 0 {
+			t.Fatal("OrDone pulling from closed stream")
+		}
+	}
+}
