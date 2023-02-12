4b3ec4bf8a20a054e52fe232ec8b265818a88023
roncewind <dad@lynntribe.net>
2023-02-12 07:25:18 -0800

    changed to the more idiomatc 'any' type

--- util/util.go
+++ util/util.go
@@ -6,7 +6,7 @@ import "context"
 
 // OrDone encapsulates the for-select idiom used for many goroutines
 // the idea is that it makes the code easier to read
-func OrDone[T interface{}](ctx context.Context, c <-chan T) <-chan T {
+func OrDone[T any](ctx context.Context, c <-chan T) <-chan T {
 	valueStream := make(chan T)
 	go func() {
 		defer close(valueStream)
