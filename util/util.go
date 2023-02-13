9ddb57120721d321b476600d227a102f9986f0b0
roncewind <dad@lynntribe.net>
2023-02-13 08:27:12 -0800

    added Tee function

--- util/util.go
+++ util/util.go
@@ -27,3 +27,30 @@ func OrDone[T any](ctx context.Context, c <-chan T) <-chan T {
 	}()
 	return valueStream
 }
+
+// ----------------------------------------------------------------------------
+
+// Tee takes one channel of objects and splits it into two channels,
+// duplicating the channels.  Much like the `tee` tool in *nix
+func Tee[T any](ctx context.Context, in <-chan T) (_, _ <-chan T) {
+	out1 := make(chan T)
+	out2 := make(chan T)
+	go func() {
+		defer close(out1)
+		defer close(out2)
+		for val := range OrDone(ctx, in) {
+			// create shadow vars
+			var out1, out2 = out1, out2
+			for i := 0; i < 2; i++ {
+				select {
+				case <-ctx.Done():
+				case out1 <- val:
+					out1 = nil
+				case out2 <- val:
+					out2 = nil
+				}
+			}
+		}
+	}()
+	return out1, out2
+}
