c5f25a88969d5d51be938d10a3a4b5df64896a8f
roncewind <dad@lynntribe.net>
2023-02-13 08:39:29 -0800

    added Or function

--- util/util.go
+++ util/util.go
@@ -54,3 +54,36 @@ func Tee[T any](ctx context.Context, in <-chan T) (_, _ <-chan T) {
 	}()
 	return out1, out2
 }
+
+// ----------------------------------------------------------------------------
+
+// Or returns a channel that combines all the specified channels.
+func Or[T any](channels ...<-chan T) <-chan T {
+	switch len(channels) {
+	case 0:
+		return nil
+	case 1:
+		return channels[0]
+	}
+
+	orDone := make(chan T)
+	go func() {
+		defer close(orDone)
+
+		switch len(channels) {
+		case 2:
+			select {
+			case <-channels[0]:
+			case <-channels[1]:
+			}
+		default:
+			select {
+			case <-channels[0]:
+			case <-channels[1]:
+			case <-channels[2]:
+			case <-Or(append(channels[3:], orDone)...):
+			}
+		}
+	}()
+	return orDone
+}
