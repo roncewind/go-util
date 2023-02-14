339920e94f132551ed44362a0de7e62628a0fe7c
roncewind <dad@lynntribe.net>
2023-02-14 12:39:18 -0800

    added RepeatFn generator tests

--- util/util_test.go
+++ util/util_test.go
@@ -281,6 +281,40 @@ func TestUtil_Repeat_cancel(t *testing.T) {
 
 // ----------------------------------------------------------------------------
 
+// test Repeat generator
+func TestUtil_RepeatFn(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	fn := func() int {
+		return 1
+	}
+	c := RepeatFn(ctx, fn)
+	val1 := <-c
+	val2 := <-c
+	if val1 != val2 {
+		t.Fatal("function results were not repeated as expected")
+	}
+}
+
+// ----------------------------------------------------------------------------
+
+// test Repeat generator with cancelled context
+func TestUtil_RepeatFn_cancel(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+	fn := func() int {
+		return 1
+	}
+	c := RepeatFn(ctx, fn)
+	_, ok1 := <-c
+	_, ok2 := <-c
+	if ok1 && ok2 {
+		t.Fatal("cancel RepeatFn failed")
+	}
+}
+
+// ----------------------------------------------------------------------------
+
 // test Take generator
 func TestUtil_Take(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
