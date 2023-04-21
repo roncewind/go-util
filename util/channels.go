2d9e9cc1f28e3ae5a40e2602d79c90a27fcbbdeb
roncewind <dad@lynntribe.net>
2023-04-21 11:56:50 -0700

    race condition fixes

--- util/channels.go
+++ util/channels.go
@@ -2,11 +2,14 @@ package util
 
 import (
 	"context"
+	"math"
+	"math/rand"
 	"sync"
+	"time"
 )
 
 // ----------------------------------------------------------------------------
-// Channels based on Katherine Cox-Buday's "Concurrency in Go"
+// Channels some of which is based on Katherine Cox-Buday's "Concurrency in Go"
 // ----------------------------------------------------------------------------
 
 // ----------------------------------------------------------------------------
@@ -159,4 +162,136 @@ func Bridge[T any](ctx context.Context, chanStream <-chan <-chan T) <-chan T {
 		}
 	}()
 	return valStream
+}
+
+// ----------------------------------------------------------------------------
+
+// Shuffle items coming through a channel.
+// input a channel of items to be shuffled.
+// output a channel of shuffled items.
+// target distance is how, on average, far each item should move.
+// there's no guarantee that a particular item will move that far, but it
+// is used as general guidance.
+// Beware: This function incurs a 1μs per item performance hit.
+func Shuffle[T any](ctx context.Context, in chan T, targetDistance int) <-chan T {
+
+	bufferSize = int(math.Round(float64(targetDistance) * 1.7))
+	if bufferSize <= 0 {
+		bufferSize = 1
+	}
+	// fmt.Println("bufferSize:", bufferSize)
+	count := 0
+	recordChan := make(chan *record, 5)
+	outChan := make(chan T)
+	go func() {
+		for item := range OrDone(ctx, in) {
+			count++
+			r := record{
+				item:         item,
+				count:        0,
+				initPosition: count,
+			}
+			recordChan <- &r
+		}
+		close(recordChan)
+		// fmt.Println("Total records received:", count)
+	}()
+	go doShuffle(ctx, recordChan, outChan)
+	return outChan
+}
+
+// ----------------------------------------------------------------------------
+
+// internal type for tracking the record
+type record struct {
+	item         any
+	count        int
+	initPosition int
+}
+
+var bufferSize int = 1000
+var delayInMicros time.Duration = 1
+
+// ----------------------------------------------------------------------------
+
+// shuffle the records
+func doShuffle[T any](ctx context.Context, in chan *record, out chan T) {
+
+	var wg sync.WaitGroup
+	readCount := 0
+	writeCount := 0
+	distance := 0
+	var sliceMutex sync.RWMutex
+	recordBuffer := make([]*record, bufferSize)
+	getRecordPtr := func(slot int) *record {
+		sliceMutex.RLock()
+		rptr := recordBuffer[slot]
+		sliceMutex.RUnlock()
+		return rptr
+	}
+	doneReading := make(chan struct{})
+	wg.Add(2)
+	// read from the in channel randomly putting records in slice slots
+	go func() {
+		r := rand.New(rand.NewSource(time.Now().Unix()))
+		for item := range OrDone(ctx, in) {
+			readCount++
+			slot := r.Intn(bufferSize)
+			for getRecordPtr(slot) != nil {
+				slot = r.Intn(bufferSize)
+			}
+			sliceMutex.Lock()
+			recordBuffer[slot] = item
+			sliceMutex.Unlock()
+		}
+		doneReading <- struct{}{}
+		wg.Done()
+	}()
+
+	// randomly read records from slice slots and put them in the out channel
+	go func() {
+		r := rand.New(rand.NewSource(time.Now().Unix()))
+		var item *record
+	stillReading:
+		for {
+			select {
+			case <-doneReading:
+				break stillReading
+			default:
+				slot := r.Intn(bufferSize)
+				for getRecordPtr(slot) == nil {
+					slot = r.Intn(bufferSize)
+				}
+
+				sliceMutex.Lock()
+				item, recordBuffer[slot] = recordBuffer[slot], nil
+				sliceMutex.Unlock()
+				writeCount++
+				distance += int(math.Abs(float64(writeCount - item.initPosition)))
+				// pause before each round to allow the writer to fill in the slice
+				time.Sleep(delayInMicros * time.Microsecond)
+				// fmt.Println(item.initPosition, "-->", writeCount, "d=", writeCount-item.initPosition)
+				out <- item.item.(T)
+
+			}
+		}
+		// flush the rest from the buffer
+		sliceMutex.Lock()
+		for i, item := range recordBuffer {
+			if item != nil {
+				writeCount++
+				distance += int(math.Abs(float64(writeCount - item.initPosition)))
+				// fmt.Println(item.initPosition, "-->", writeCount, "d=", writeCount-item.initPosition)
+				out <- item.item.(T)
+				recordBuffer[i] = nil
+			}
+		}
+		sliceMutex.Unlock()
+		close(out)
+		wg.Done()
+	}()
+	wg.Wait()
+	// fmt.Println("Total records written:", writeCount)
+	// fmt.Println("Total distance:", distance)
+	// fmt.Println("Average distance:", distance/writeCount)
 }
--- util/channels_test.go
+++ util/channels_test.go
@@ -249,3 +249,63 @@ func TestUtil_Bridge_cancel(t *testing.T) {
 		t.Fatal("error in Bridge with cancelled context")
 	}
 }
+
+// ----------------------------------------------------------------------------
+
+// test Shuffle
+func TestUtil_Shuffle(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// generate values from 0 to n
+	n := 100000
+	generateValues := func() chan int {
+		stream := make(chan int)
+		go func() {
+			defer close(stream)
+			for i := 1; i <= n; i++ {
+				stream <- i
+			}
+		}()
+		return stream
+	}
+
+	accumulator := 0
+	for item := range Shuffle(ctx, generateValues(), 10000) {
+		accumulator += item
+	}
+
+	if accumulator != (n*(n+1))/2 {
+		t.Fatal("error in Shuffle")
+	}
+}
+
+// ----------------------------------------------------------------------------
+
+// test Shuffle
+func TestUtil_Shuffle_bad_distance(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// generate values from 0 to n
+	n := 100000
+	generateValues := func() chan int {
+		stream := make(chan int)
+		go func() {
+			defer close(stream)
+			for i := 1; i <= n; i++ {
+				stream <- i
+			}
+		}()
+		return stream
+	}
+
+	accumulator := 0
+	for item := range Shuffle(ctx, generateValues(), 0) {
+		accumulator += item
+	}
+
+	if accumulator != (n*(n+1))/2 {
+		t.Fatal("error in Shuffle")
+	}
+}
